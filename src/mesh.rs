30227fc4d74de016e1a70141f0db7a74aa4aa728
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2021-03-07 12:44:48 -0500

    ZSortedFacets

--- src/mesh.rs
+++ src/mesh.rs
@@ -85,4 +85,106 @@ impl Mesh {
             facet.translate(&translation);
         }
     }
+
+    fn lower_z_bound(&self) -> i64 {
+        self.facets.iter().map(|facet| facet.lower_z_bound()).min().unwrap()
+    }
+
+    pub fn zsort_facets(self) -> ZSortedFacets {
+        let lower_z_bound = self.lower_z_bound();
+        ZSortedFacets::new(self.facets, lower_z_bound)
+    }
+}
+
+struct CachedFacetBounds {
+    facet: Facet,
+    lower_bound: i64,
+    upper_bound: i64,
+}
+
+impl CachedFacetBounds {
+    fn new(facet: Facet) -> Self {
+        Self {
+            lower_bound: facet.lower_z_bound(),
+            upper_bound: facet.upper_z_bound(),
+            facet,
+        }
+    }
+}
+
+pub struct FacetIntersections<'a> {
+    facets: std::slice::Iter<'a, CachedFacetBounds>,
+}
+
+impl<'a> FacetIntersections<'a> {
+    fn new(facets: &'a [CachedFacetBounds]) -> Self {
+        Self {
+            facets: facets.iter(),
+        }
+    }
+}
+
+impl<'a> Iterator for FacetIntersections<'a> {
+    type Item = &'a Facet;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.facets.next().map(|i| &i.facet)
+    }
+}
+
+pub struct ZSortedFacets {
+    /// All facets, sorted by lower bound in descending order
+    facets: Vec<CachedFacetBounds>,
+    current_height: i64,
+}
+
+impl ZSortedFacets {
+    fn new(facets: Vec<Facet>, start_height: i64) -> Self {
+        let mut facets: Vec<CachedFacetBounds> = facets.into_iter().map(CachedFacetBounds::new).collect();
+        // compare b to a so it sorts in descending order
+        facets.sort_unstable_by(|a, b| b.lower_bound.cmp(&a.lower_bound));
+
+        let mut this = Self {
+            facets,
+            current_height: start_height,
+        };
+        // advance height by 0 to trim any facets that are already below the start height
+        this.advance_height(0);
+
+        this
+    }
+
+    /// Increases the current height by `increment` and trims facets whose upper bounds
+    /// are below the new height.
+    ///
+    /// `increment` is unsigned because the height can't be decreased - facets below the
+    /// current height have already been trimmed,
+    pub fn advance_height(&mut self, increment: u64) {
+        self.current_height += increment as i64;
+        for i in self.facets.iter().enumerate().rev()
+            .take_while(|(_, facet)| facet.lower_bound < self.current_height)
+            .filter(|(_, facet)| facet.upper_bound < self.current_height)
+            .map(|(index, _)| index)
+        {
+            todo!("trim");
+        }
+    }
+
+    /// Returns an iterator over all facets that intersect with a plane at the current height
+    pub fn facet_intersections(&self) -> FacetIntersections {
+        let first_facet_above = self.facets.iter().enumerate().rev()
+            .find(|(_, facet)| facet.lower_bound > self.current_height)
+            .map(|(index, _)| index);
+
+        if let Some(index) = first_facet_above {
+            FacetIntersections::new(&self.facets[index + 1..])
+        } else {
+            FacetIntersections::new(&self.facets[..])
+        }
+    }
+
+    /// Returns true if there are no more facet vertices at or above the current height
+    pub fn done(&self) -> bool {
+        self.facets.is_empty()
+    }
 }
